85bec798a20e2f35d0159e1d711219e1456874de
GodPepe7 <privatalexm@gmail.com>
2023-10-29 19:06:35 +0100

    Implement scoring system

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import CardField from "./components/CardField";
+import "./index.css";
+
 function App() {
-  return <></>;
+  const [highScore, setHighScore] = useState(0);
+  const [alreadyClicked, setAlreadyClicked] = useState([]);
+
+  const addAlreadyClicked = (id) => {
+    if (alreadyClicked.includes(id)) {
+      setAlreadyClicked([]);
+    } else {
+      setAlreadyClicked([...alreadyClicked, id]);
+    }
+  };
+
+  if (highScore < alreadyClicked.length) {
+    setHighScore(alreadyClicked.length);
+  }
+
+  return (
+    <>
+      <h1>Score: {alreadyClicked.length}</h1>
+      <h1>High Score: {highScore}</h1>
+      <CardField addAlreadyClicked={addAlreadyClicked}></CardField>
+    </>
+  );
 }
 
 export default App;
